71f7f53a40c9fda7906e6affdcfec048194ad409
lab357 <lab357@georgetown.edu>
2023-10-25 17:36:17 -0400

    Run example script

---
+++ app/my_script.py
@@ -0,0 +1,27 @@
+# this is the "app/my_script.py" file...
+
+print("HELLO WORLD!")
+
+x = 2 + 2
+print(x)
+
+#Setup
+
+#Create and activate a virtual environment:
+
+'''sh
+
+conda create -n my-first-env python=3.10
+
+conda activate my-first-env
+
+'''
+
+
+
+## Usage
+#Run the example script:
+
+'''sh
+python app/my_script.py
+'''
